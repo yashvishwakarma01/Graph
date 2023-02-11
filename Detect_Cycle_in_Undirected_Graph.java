0a0b11b22f50c7449f02b4acbba6041162a5a97e
yashvishwakarma01 <120748037+yashvishwakarma01@users.noreply.github.com>
2023-02-11 21:18:12 +0530

    Create Detect_Cycle_in_Undirected_Graph.java

---
+++ Detect_Cycle_in_Undirected_Graph.java
@@ -0,0 +1,49 @@
+class Pair{
+    int key;
+    int value;
+    Pair(int key,int value){
+        this.key=key;
+        this.value=value;
+    }
+}
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+       boolean [] visited=new boolean[V];
+       Queue<Pair> q=new LinkedList<>();
+       boolean b =false;
+       for(int i=0;i<V;i++){
+           if(!visited[i]){
+                b=bfs(adj,q,i,visited);
+               if(b){
+                   break;
+                   
+               }
+           }
+       }
+       return b;
+       
+    }
+    
+    public boolean bfs(ArrayList<ArrayList<Integer>> adj, Queue<Pair> q,int i,boolean []visited){
+        boolean b =false;
+        visited[i]=true;
+        q.offer(new Pair(i,-1));
+        while(!q.isEmpty()){
+            Pair u=q.poll();
+            for(int element:adj.get(u.key)){
+                if(element!=u.value && !visited[element]){
+                    q.offer(new Pair(element,u.key));
+                    visited[element]=true;
+                }
+                else if(element!=u.value && visited[element]==true){
+                    b=true;
+                }
+            }
+            
+            
+            
+        }
+        return b;
+    }
+}
